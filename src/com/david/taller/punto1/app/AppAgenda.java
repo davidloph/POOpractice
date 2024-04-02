174d7cf5477512dc7282023b4aba3efa48cfa489
davidlop <davidloperahenao@gmail.com>
2024-04-02 18:05:38 -0500

    Maquina Dispensadora

---
+++ src/com/david/taller/punto1/app/AppAgenda.java
@@ -0,0 +1,14 @@
+package com.david.taller.punto1.app;
+
+import com.david.taller.punto1.dominio.Contacto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AppAgenda {
+    public static void main(String[] args) {
+        List<Contacto> contactos = new ArrayList<>();
+
+
+    }
+}
---
+++ src/com/david/taller/punto1/dominio/Agenda.java
@@ -0,0 +1,40 @@
+package com.david.taller.punto1.dominio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Agenda {
+    private List<Contacto> contactos;
+    public Agenda(){
+        this,contactos = new ArrayList<>();
+    }
+
+    public void crearContacto(String nombre, String apellido, long celular){
+         Contacto contacto = new Contacto()
+
+    }
+
+    public void eliminarContacto(long celular){
+
+    }
+
+    public List<Contacto> buscar(String nombre){
+        return null;
+    }
+
+    public List<Contacto> buscarPorApellido(String apellido){
+        return null;
+    }
+
+    public Contacto buscar(long celular){
+        return null;
+    }
+
+    public void cambiarCelular(long viejoCelular, long nuevoCelular){
+
+    }
+
+    public List<Contacto> ordenar(){
+        return null;
+    }
+}
